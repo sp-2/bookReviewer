e349a83e8cd124aad700d271040db5cda55d4bb5
sp-2 <47649293+sp-2@users.noreply.github.com>
2019-02-16 19:31:11 -0800

    Update urls.py

--- main/apps/beltReviewer/urls.py
+++ main/apps/beltReviewer/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views           # This line is new!
+from . import views          
 urlpatterns = [
 	url(r'^$', views.index),     # Root route
     url(r'^process/register$', views.registerUser),

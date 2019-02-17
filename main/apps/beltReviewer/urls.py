79deb39d50fecb367f1a30559ee31970c3b3ea56
sp-2 <47649293+sp-2@users.noreply.github.com>
2019-02-16 19:33:36 -0800

    Update urls.py

--- main/apps/beltReviewer/urls.py
+++ main/apps/beltReviewer/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views          
 urlpatterns = [
 	url(r'^$', views.index),     # Root route
-    url(r'^process/register$', views.registerUser),
-    url(r'^process/login$', views.loginUser),
-    url(r'^books$', views.books),#success
-    url(r'^process/logout$', views.logout),
+    	url(r'^process/register$', views.registerUser),
+    	url(r'^process/login$', views.loginUser),
+    	url(r'^books$', views.books),#success
+    	url(r'^process/logout$', views.logout),
 	url(r'^books/add$', views.show_books_add),
 	url(r'^books/(?P<id>[0-9]+)$', views.show_books),
 	url(r'^process/add_book_add_review$', views.process_add_book_add_review),
